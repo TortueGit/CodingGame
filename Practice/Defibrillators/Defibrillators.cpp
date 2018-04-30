d040d88a8f6f987896c49ecef1d7e5ac753562b5
TortueGit <a.peyrard@gmail.com>
2018-04-30 16:13:07 +0200

    Début exercice Defibrillators

---
+++ Practice/Defibrillators/Defibrillators.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+int main()
+{
+    string LON;
+    cin >> LON; cin.ignore();
+    string LAT;
+    cin >> LAT; cin.ignore();
+    int N;
+    cin >> N; cin.ignore();
+    for (int i = 0; i < N; i++) {
+        string DEFIB;
+        getline(cin, DEFIB);
+    }
+
+    // Write an action using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "answer" << endl;
+}
