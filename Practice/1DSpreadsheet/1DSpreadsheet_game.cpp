7857eb2422a7d85ac25cc65e5be085a04f9bfed3
arthur <arthur@TurtleDebian.home>
2020-05-19 18:32:10 +0200

    Refactorisation & correction. All tests passed.

--- Practice/1DSpreadsheet/1DSpreadsheet_game.cpp
+++ Practice/1DSpreadsheet/1DSpreadsheet_game.cpp
@@ -11,6 +11,11 @@ using namespace std;
  **/
 
 void CalculValue(vector<vector<string>> &_operAndArgs, int _index);
+void ReadValue(vector<vector<string>> &_operAndArgs, int _index);
+void AddValue(vector<vector<string>> &_operAndArgs, int _index);
+void SubValue(vector<vector<string>> &_operAndArgs, int _index);
+void MultValue(vector<vector<string>> &_operAndArgs, int _index);
+int GetIndex(string cellValue);
 
 int main()
 {
@@ -31,6 +36,8 @@ int main()
         // Read the entries, and stack them in the vector of string.
         cin >> operAndArgs[i][0] >> operAndArgs[i][1] >> operAndArgs[i][2];
         cin.ignore();
+
+        cerr << operAndArgs[i][0] << operAndArgs[i][1] << operAndArgs[i][2] << endl;
 
         // Calculated value is initialized to "" and will be calculated later.
         operAndArgs[i][3] = "";
@@ -55,112 +62,151 @@ int main()
 
 void CalculValue(vector<vector<string>> &_operAndArgs, int _index)
 {
+	// Reading cell operation to know what to do.
     if (_operAndArgs[_index][0].compare("VALUE") == 0)
     {
-        if (_operAndArgs[_index][1].rfind('$', 0) == 0)
-        {
-            int index = atoi(_operAndArgs[_index][1].substr(1).c_str());
-            if (_operAndArgs[index][3].empty())
-                CalculValue(_operAndArgs, index);
-
-            _operAndArgs[_index][3] = atoi(_operAndArgs[index][3].c_str());
-        }
-        else
-            _operAndArgs[_index][3] = _operAndArgs[_index][1];
+    	ReadValue(_operAndArgs, _index);
     }
     else if (_operAndArgs[_index][0].compare("ADD") == 0)
     {
-        int val1;
-        int val2;
-        if (_operAndArgs[_index][1].rfind('$', 0) == 0)
-        {
-            int index = atoi(_operAndArgs[_index][1].substr(1).c_str());
-            if (_operAndArgs[index][3].empty())
-                CalculValue(_operAndArgs, index);
-
-            val1 = atoi(_operAndArgs[index][3].c_str());
-        }
-        else
-        {
-            val1 = atoi(_operAndArgs[_index][1].c_str());
-        }
-
-        if (_operAndArgs[_index][2].rfind('$', 0) == 0)
-        {
-            int index = atoi(_operAndArgs[_index][2].substr(1).c_str());
-            if (_operAndArgs[index][3].empty())
-                CalculValue(_operAndArgs, index);
-
-            val2 = atoi(_operAndArgs[index][3].c_str());
-        }
-        else
-        {
-            val2 = atoi(_operAndArgs[_index][2].c_str());
-        }
-        _operAndArgs[_index][3] = to_string(val1+val2);
+    	AddValue(_operAndArgs, _index);
     }
-    if (_operAndArgs[_index][0].compare("SUB") == 0)
+    else if (_operAndArgs[_index][0].compare("SUB") == 0)
     {
-        int val1;
-        int val2;
-        if (_operAndArgs[_index][1].rfind('$', 0) == 0)
-        {
-            int index = atoi(_operAndArgs[_index][1].substr(1).c_str());
-            if (_operAndArgs[index][3].empty())
-                CalculValue(_operAndArgs, index);
-
-            val1 = atoi(_operAndArgs[index][3].c_str());
-        }
-        else
-        {
-            val1 = atoi(_operAndArgs[_index][1].c_str());
-        }
-
-        if (_operAndArgs[_index][2].rfind('$', 0) == 0)
-        {
-            int index = atoi(_operAndArgs[_index][2].substr(1).c_str());
-            if (_operAndArgs[index][3].empty())
-                CalculValue(_operAndArgs, index);
-
-            val2 = atoi(_operAndArgs[index][3].c_str());
-        }
-        else
-        {
-            val2 = atoi(_operAndArgs[_index][2].c_str());
-        }
-        _operAndArgs[_index][3] = to_string(val1-val2);
+    	SubValue(_operAndArgs, _index);
     }
-    if (_operAndArgs[_index][0].compare("MULT") == 0)
+    else if (_operAndArgs[_index][0].compare("MULT") == 0)
     {
-        int val1;
-        int val2;
-        if (_operAndArgs[_index][1].rfind('$', 0) == 0)
-        {
-            int index = atoi(_operAndArgs[_index][1].substr(1).c_str());
-            if (_operAndArgs[index][3].empty())
-                CalculValue(_operAndArgs, index);
-
-            val1 = atoi(_operAndArgs[index][3].c_str());
-        }
-        else
-        {
-            val1 = atoi(_operAndArgs[_index][1].c_str());
-        }
-
-        if (_operAndArgs[_index][2].rfind('$', 0) == 0)
-        {
-            int index = atoi(_operAndArgs[_index][2].substr(1).c_str());
-            if (_operAndArgs[index][3].empty())
-                CalculValue(_operAndArgs, index);
-
-            val2 = atoi(_operAndArgs[index][3].c_str());
-        }
-        else
-        {
-            val2 = atoi(_operAndArgs[_index][2].c_str());
-        }
-        _operAndArgs[_index][3] = to_string(val1*val2);
+    	MultValue(_operAndArgs, _index);
     }
     cerr << "_index[" << _index << "]" << _operAndArgs[_index][0] << "_" << _operAndArgs[_index][1] << "_" << _operAndArgs[_index][2] << endl;
     cerr << "_operAndArgs[" << _index << "][3] = " << _operAndArgs[_index][3] << endl;
+}
+
+void ReadValue(vector<vector<string>> &_operAndArgs, int _index)
+{
+	// Easy one, we have the direct value.
+	// But maybe it is the value of an other cell so we have to check it.
+	if (_operAndArgs[_index][1].rfind('$', 0) == 0)
+	{
+		// It is a referenced value, let's go find the real value.
+		int index = GetIndex(_operAndArgs[_index][1]);
+		// The value of the referenced cell have to be calculated, so we go for it.
+		if (_operAndArgs[index][3].empty())
+			CalculValue(_operAndArgs, index);
+
+		// We have calculate the referenced cell so we can write the value in the current cell.
+		_operAndArgs[_index][3] = _operAndArgs[index][3];
+	}
+	else
+	{
+		// No referenced cell, we write the value.
+		_operAndArgs[_index][3] = _operAndArgs[_index][1];
+	}
+}
+
+void AddValue(vector<vector<string>> &_operAndArgs, int _index)
+{
+	// Operation ADD, so we have to calculate arg1+arg2.
+	int val1;
+	int val2;
+	if (_operAndArgs[_index][1].rfind('$', 0) == 0)
+	{
+		// arg1 is a referenced cell value, so go get this value.
+		int index = GetIndex(_operAndArgs[_index][1]);
+		if (_operAndArgs[index][3].empty())
+			CalculValue(_operAndArgs, index);
+
+		val1 = atoi(_operAndArgs[index][3].c_str());
+	}
+	else
+	{
+		val1 = atoi(_operAndArgs[_index][1].c_str());
+	}
+
+	if (_operAndArgs[_index][2].rfind('$', 0) == 0)
+	{
+		// arg2 is a referenced cell value, so go get this value.
+		int index = GetIndex(_operAndArgs[_index][2]);
+		if (_operAndArgs[index][3].empty())
+			CalculValue(_operAndArgs, index);
+
+		val2 = atoi(_operAndArgs[index][3].c_str());
+	}
+	else
+	{
+		val2 = atoi(_operAndArgs[_index][2].c_str());
+	}
+	_operAndArgs[_index][3] = to_string(val1+val2);
+}
+
+void SubValue(vector<vector<string>> &_operAndArgs, int _index)
+{
+	// Operation SUB, so we have to calculate arg1-arg2.
+	int val1;
+	int val2;
+	if (_operAndArgs[_index][1].rfind('$', 0) == 0)
+	{
+		int index = GetIndex(_operAndArgs[_index][1]);
+		if (_operAndArgs[index][3].empty())
+			CalculValue(_operAndArgs, index);
+
+		val1 = atoi(_operAndArgs[index][3].c_str());
+	}
+	else
+	{
+		val1 = atoi(_operAndArgs[_index][1].c_str());
+	}
+
+	if (_operAndArgs[_index][2].rfind('$', 0) == 0)
+	{
+		int index = GetIndex(_operAndArgs[_index][2]);
+		if (_operAndArgs[index][3].empty())
+			CalculValue(_operAndArgs, index);
+
+		val2 = atoi(_operAndArgs[index][3].c_str());
+	}
+	else
+	{
+		val2 = atoi(_operAndArgs[_index][2].c_str());
+	}
+	_operAndArgs[_index][3] = to_string(val1-val2);
+}
+
+void MultValue(vector<vector<string>> &_operAndArgs, int _index)
+{
+	// Operation MULT, so we have to calculate arg1*arg2.
+	int val1;
+	int val2;
+	if (_operAndArgs[_index][1].rfind('$', 0) == 0)
+	{
+		int index = GetIndex(_operAndArgs[_index][1]);
+		if (_operAndArgs[index][3].empty())
+			CalculValue(_operAndArgs, index);
+
+		val1 = atoi(_operAndArgs[index][3].c_str());
+	}
+	else
+	{
+		val1 = atoi(_operAndArgs[_index][1].c_str());
+	}
+
+	if (_operAndArgs[_index][2].rfind('$', 0) == 0)
+	{
+		int index = GetIndex(_operAndArgs[_index][2]);
+		if (_operAndArgs[index][3].empty())
+			CalculValue(_operAndArgs, index);
+
+		val2 = atoi(_operAndArgs[index][3].c_str());
+	}
+	else
+	{
+		val2 = atoi(_operAndArgs[_index][2].c_str());
+	}
+	_operAndArgs[_index][3] = to_string(val1*val2);
+}
+
+int GetIndex(string cellValue)
+{
+	return atoi(cellValue.substr(1).c_str());
 }
